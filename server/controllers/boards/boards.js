5e896aace9e854e395ca1a951bbdfd6edf19369f
minjman2659 <minjman2659@gmail.com>
2021-10-22 16:42:36 +0900

    [Server] boards controller boards.js 목록 조회 pagination 구현

--- server/controllers/boards/boards.js
+++ server/controllers/boards/boards.js
@@ -1,5 +1,4 @@
-const usersList = require('../../models/usersList');
-let boardsList = require('../../models/boardsList');
+const boardsList = require('../../models/boardsList');
 const {
   generateAccessToken,
   isAuthorized,
@@ -8,6 +7,40 @@ const { refreshAuthorized } = require('../tokenFunction/refreshToken');
 
 module.exports = {
   get: (req, res) => {
-    res.send('this is boards/get');
+    const offset = Number(req.query.offset);
+    const limit = Number(req.query.limit);
+    const { userId } = req.query;
+
+    if (userId) {
+      //! 내가 쓴 글 목록 조회 API
+      const accessTokenCheck = isAuthorized(req);
+      const refreshTokenCheck = refreshAuthorized(req);
+      if (!accessTokenCheck) {
+        // accessToken 만료 / refreshToken 만료 (401)
+        if (!refreshTokenCheck) {
+          res.status(401).json({ message: 'Send new Login Request' });
+        }
+        // accessToken 만료 / refreshToken 유효 (201)
+        else {
+          delete refreshTokenCheck.exp;
+          const accessToken = generateAccessToken(refreshTokenCheck);
+          const myData = boardsList.filter(
+            (el) => el.userId === Number(userId)
+          );
+          const returnData = myData.slice(offset, offset + limit);
+          res.status(201).json({ accessToken, data: returnData });
+        }
+      }
+      // accessToken 유효 (200)
+      else {
+        const myData = boardsList.filter((el) => el.userId === Number(userId));
+        const returnData = myData.slice(offset, offset + limit);
+        res.status(200).json({ data: returnData });
+      }
+    } else {
+      //! 전체 글 목록 조회 API
+      const returnData = boardsList.slice(offset, offset + limit);
+      res.status(200).json({ data: returnData });
+    }
   },
 };
